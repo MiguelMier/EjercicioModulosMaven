771ebf29290a3e20e643042fedb4e4066f64ae42
Miguel Mier <miguel.mier@babelgroup.com>
2024-03-07 13:39:48 +0100

    Cambios de paquetes y mvn install

--- src/main/java/es/babel/Main.java
+++ src/main/java/es/babel/Main.java
@@ -1,10 +1,11 @@
 package es.babel;
 
-
+import es.babel.applications.AhorcadoApplication;
+import es.babel.configuration.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class ApplicationRunner {
 
+public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         AhorcadoApplication ahorcado = context.getBean(AhorcadoApplication.class);
